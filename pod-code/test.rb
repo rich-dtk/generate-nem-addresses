63f35453bfc8ccb8a504938c0ad2010fc29eece5
Rich Pelavin <rich@reactor8.com>
2018-07-14 20:10:27 +0000

    update

---
+++ pod-code/test.rb
@@ -0,0 +1,12 @@
+require('kubeclient')
+ssl_options = { verify_ssl: OpenSSL::SSL::VERIFY_NONE }
+auth_options = {
+  bearer_token_file: '/var/run/secrets/kubernetes.io/serviceaccount/token'
+}
+client = Kubeclient::Client.new(
+  'https://kubernetes.default.svc:443/api/', 'v1', ssl_options: ssl_options,  auth_options: auth_options
+)
+namespace = File.read('/var/run/secrets/kubernetes.io/serviceaccount/namespace')
+require 'pp'
+pp "namespace = #{namespace}"
+pp client.get_pods(namespace: namespace)
