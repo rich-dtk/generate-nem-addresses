00a0a0a5a2babef8b41ed7ffa775824482d7a69a
Rich Pelavin <rich@reactor8.com>
2018-07-15 00:21:43 +0000

    update

--- pod-code/bin/generate_addresses.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby
-require_relative('../catapult')
-# input_file_path = ARGV[0]
-input_file_path = '/tmp/raw_addresses_path.txt'
-# TODO: check if configmap exists and if so. no op
-ruby_object = Catapult::Addresses.parse(input_file_path)
-# TODO: write to config map
-require 'pp'
-pp ruby_object
